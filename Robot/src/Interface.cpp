ed946b023c029380bc64cb05053854a9d3fffb2a
FMenziesENG <36442745+FMenziesENG@users.noreply.github.com>
2018-04-15 10:45:26 +0100

    Update Interface.cpp

--- Robot/src/Interface.cpp
+++ Robot/src/Interface.cpp
@@ -1,13 +1,9 @@
 #include <curses.h>
 #include <string>
 #include <unistd.h>
-//#include <stdio.h>
 #include <Location.h>
 
 //Reads Robot Sensor Data and Displays
-//Reads Motor Status and Displays
-
-//recieves commands and uses to control motors
 
 LSM303AGR lsm;
 int fd = lsm.setup();
@@ -23,23 +19,16 @@ void Interface()
 	scrollok(stdscr, TRUE);
 	nodelay(stdscr, TRUE);
 
-	Location dis;
-//	dis.Magno_Init();
+	Location dis; //Display location
 	while(1)
 	{
-
-		input = getch();
 		erase();	//clears screen
 
 		printw("------------Robopot-Display------------\n"
 		"------------Sensor-Display------------\n"
-//		"Proximity Sensor Data: %i\n"
-		"Magnemometer Sensor Data: %i\n"
-		//"GUI Input: %c\n",
-		//"------------Motor-Display------------\n"
-		//"Motor 1: %i\n"
-		//"Motor 2: %i\n", 
-//		      , dis.Find_Proximity()
+		"Proximity Sensor Data: %i\n"
+		"Magnemometer Sensor Data: %i\n" 
+		      , dis.Find_Proximity()
 			,dis.Find_Direction(fd)
 		);
 
