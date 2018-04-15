2869727428da7c9684adc9e40a11e46c554c22d2
FMenziesENG <36442745+FMenziesENG@users.noreply.github.com>
2018-04-15 10:52:29 +0100

    Update GUI.cpp

--- Robot/src/GUI.cpp
+++ Robot/src/GUI.cpp
@@ -8,29 +8,22 @@ GUI::GUI()
 GUI::~GUI()
 {
 }
-
-int GUI::Setup()
-{
-	Send(SetupFile, SetupMessage);
-}
-
+ //Checks to see if command file is readable
 bool GUI::Check_Cmd(const std::string& filename)
 {
 	std::ifstream file(filename.c_str());
 	return file.good();
 }
-
+//Reads command file and returns pot destination
 char GUI::Read_Cmd(const std::string& filename)
 {
 	std::ifstream file(filename.c_str());
 	file.get(cmd);
-//	remove(filename.c_str());
 	return cmd;
 }
-
+//Writes to file to inform GUI of present location
 void GUI::Send(const std::string& filename, const std::string& message)
 {
 	std::ifstream file;
 	file.open(filename.c_str());
-	std::cout << message;
 }
